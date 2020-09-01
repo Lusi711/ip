28fa574a82f8d1e8d1f8b6c4e032aaa6e46173de
Lusi711 <e0424985@u.nus.edu>
2020-09-01 19:03:59 +0800

    Add Level-1: * Split Greet() into enterGreet() and farewellGreet() * Echo() echos commands entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void Greet(String line) {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(line);
-            if (i == 0) {
-                System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
-            }
-            else if (i == 1){
-                System.out.println("Bye. Hope to see you again soon!\n");
-            }
-        }
+
+    public static String indentLine = "----------------------------------------";
+
+    public static void enterGreet() {
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        System.out.println(indentLine);
     }
+
+    public static void farewellGreet() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(indentLine);
+    }
+
+    public static void Echo() {
+        String line = null;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.print(indentLine + "\n" + line + "\n" + indentLine + "\n");
+            line = in.nextLine();
+        }
+        System.out.println(indentLine);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,6 +33,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String indentation = "----------------------------------------";
         System.out.println("Hello from\n" + logo);
-        Greet(indentation);
+        System.out.println(indentLine);
+
+        //Level-0
+        enterGreet();
+        farewellGreet();
+        enterGreet();
+
+        //Level-1
+        Echo();
+
+        farewellGreet();
     }
 }
