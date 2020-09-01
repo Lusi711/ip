e5519ccc68eb0586dad6debd17aefc52d37b7d9e
Lusi711 <e0424985@u.nus.edu>
2020-09-01 18:59:24 +0800

    Add Level - 0: Implement Greet() method: Greets user and exits

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    public static void Greet(String line) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(line);
+            if (i == 0) {
+                System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+            }
+            else if (i == 1){
+                System.out.println("Bye. Hope to see you again soon!\n");
+            }
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String indentation = "----------------------------------------";
         System.out.println("Hello from\n" + logo);
+        Greet(indentation);
     }
 }
