a342de1bd6c0d1fd04773c2e71164114d25ed473
Lusi711 <e0424985@u.nus.edu>
2020-09-01 21:19:10 +0800

    Add Level-3: * markAsDone() method marks Task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     public static String indentLine = "----------------------------------------";
 
     public static void enterGreet() {
+        System.out.println(indentLine);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
         System.out.println(indentLine);
     }
@@ -22,21 +23,37 @@ public class Duke {
             System.out.print(indentLine + "\n" + line + "\n" + indentLine + "\n");
             line = in.nextLine();
         }
+        System.out.println(indentLine);
     }
 
-    public static void addList(String[] items, String task, int numberOfTasks) {
-        System.out.print(indentLine + "\n");
+    public static void addList(Task[] items, String task, int numberOfTasks) {
+        System.out.println(indentLine);
 
-        items[numberOfTasks] = task;
+        items[numberOfTasks] = new Task(task);
         System.out.println("added: " + task);
 
         System.out.println(indentLine);
     }
 
-    public static void displayList(String[] items) {
+    public static void markAsDone(Task[] items, String line) {
+        System.out.println(indentLine);
+
+        System.out.println("Nice! I've marked this task as done: ");
+        int itemIndex = Integer.parseInt(line.substring(line.length() - 1));
+        items[itemIndex - 1].markAsDone();
+        Task t = items[itemIndex - 1];
+        System.out.println("  " + t.getStatusIcon() + " " + t.description);
+
+        System.out.println(indentLine);
+    }
+
+    public static void displayList(Task[] items) {
+        System.out.println(indentLine);
+
+        System.out.println("Here are the tasks in your list:");
         int itemIndex = 1;
-        for (String item : items) {
-            System.out.println(itemIndex + ". " + item + "\n");
+        for (Task item : items) {
+            System.out.println(itemIndex + ". " + item.getStatusIcon() + " " + item.description);
             itemIndex++;
         }
         System.out.println(indentLine);
@@ -49,19 +66,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(indentLine);
 
         //Level-0
         enterGreet();
 
-        //Level-2
+        //Level-3
         Scanner in = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         String line = in.nextLine();
         int numberOfTasks = 0;
         while (!line.equals("bye")) {
             if (line.equals("list")) {
                 displayList(Arrays.copyOf(items, numberOfTasks));
+            } else if (line.contains("done")) {
+                markAsDone(items, line);
             } else {
                 addList(items, line, numberOfTasks);
                 numberOfTasks++;
