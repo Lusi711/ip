ace6c4ed0f6fef67ffde3c2bca6a57a09fa28f56
Lusi711 <e0424985@u.nus.edu>
2020-09-20 17:35:18 +0800

    Extract Ui class: deals with interactions with the user

---
+++ src/main/java/duke/ui/Messages.java
@@ -0,0 +1,10 @@
+package duke.ui;
+
+public class Messages {
+    public static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_MISSING_COMMAND_FORMAT = "Please enter a valid command.";
+    public static final String MESSAGE_DISPLAY_LIST = "Here are the tasks in your list:";
+    public static final String MESSAGE_WELCOME = "Hello! I'm Duke.";
+    public static final String MESSAGE_ENQUIRY = "What can I do for you?";
+}
---
+++ src/main/java/duke/ui/Ui.java
@@ -0,0 +1,70 @@
+package duke.ui;
+
+import duke.task.Task;
+import duke.task.TaskList;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static duke.ui.Messages.*;
+
+public class Ui {
+    private static final String DIVIDER = "----------------------------------------------------------";
+
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private final Scanner in;
+    private final PrintStream out;
+
+    public Ui() {
+        this(System.in, System.out);
+    }
+
+    public Ui(InputStream in, PrintStream out) {
+        this.in = new Scanner(in);
+        this.out = out;
+    }
+
+    public String readCommand() {
+        String fullInputLine = in.nextLine().trim();
+
+        return fullInputLine;
+    }
+
+    public void showWelcomeMessage() {
+        showToUser(DIVIDER, MESSAGE_WELCOME, logo, MESSAGE_ENQUIRY, DIVIDER);
+    }
+
+    public void showGoodbyeMessage() {
+        showToUser(DIVIDER, MESSAGE_GOODBYE, DIVIDER);
+    }
+
+    public void showTaskList(ArrayList<Task> tasks) {
+        showToUser(MESSAGE_DISPLAY_LIST);
+
+        for (Task task : tasks) {
+            int taskIndex = tasks.indexOf(task) + 1;
+            showToUser(taskIndex + ". " + task);
+        }
+    }
+
+    public void showFeedbackMessage(String... feedbackToUser) {
+        showToUser(feedbackToUser);
+    }
+
+    public void showLine() {
+        showToUser(DIVIDER);
+    }
+
+    private void showToUser(String... message) {
+        for (String m : message) {
+            out.println(m);
+        }
+    }
+}
