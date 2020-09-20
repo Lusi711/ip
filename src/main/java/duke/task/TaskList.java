7ea5788999bbc475fa19e849b054282810510c7a
Lusi711 <e0424985@u.nus.edu>
2020-09-20 17:39:37 +0800

    Extract TaskList class: contains the task list

---
+++ src/main/java/duke/task/TaskList.java
@@ -0,0 +1,39 @@
+package duke.task;
+
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+    private int numberOfTasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+        numberOfTasks = 0;
+    }
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addNewTask(Task task) {
+        tasks.add(task);
+    }
+
+    public Task deleteTask(int targetIndex) {
+        Task deletedTask = tasks.get(targetIndex);
+        tasks.remove(deletedTask);
+        return deletedTask;
+    }
+
+    public Task markAsDone(int targetIndex) {
+        tasks.get(targetIndex).markAsDone();
+        return tasks.get(targetIndex);
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public int getNumberOfTasks() {
+        return tasks.size();
+    }
+}
